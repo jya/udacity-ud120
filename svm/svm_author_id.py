ca86a9071c38c94821e1d12f919e292c08e4f77e
Jason <jy2an@virginia.edu>
2021-05-14 21:34:46 -0700

    end of lesson 3

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,7 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -20,11 +20,20 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
-### your code goes here ###
-
+clf = SVC(kernel='rbf', C=10000)
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+print clf.score(features_test, labels_test)
+print "training time:", round(time()-t0, 3), "s"
+print pred
+count = 0
+for x in pred:
+	if x == 1: count += 1
+print count
 #########################################################
-
-
